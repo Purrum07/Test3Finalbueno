11fa70fe9182aede3fd778858d17c91ad64364c0
Mario Márquez <48485832+Purrum07@users.noreply.github.com>
2019-05-16 12:09:39 -0600

    Test3final

---
+++ Test3FinalBueno/Test3FinalBueno/Classes/Pregunta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3FinalBueno.Classes
+{
+    class Pregunta
+    {
+        string nombre;
+        string apellido;
+
+        public void pre()
+        {
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+
+            
+
+            Console.WriteLine("Escribe tu apellido");
+            apellido = Console.ReadLine();
+
+            try
+            {
+                if (nombre.Length  <= 2 && apellido.Length <= 2)
+                {
+                    throw new System.ArgumentException();
+                }
+                
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("nombre/apellido no válido");
+            }
+
+        }
+
+    }
+        
+}
---
+++ Test3FinalBueno/Test3FinalBueno/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test3FinalBueno.Classes;
+
+namespace Test3FinalBueno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pregunta p1 = new Pregunta();
+
+            p1.pre();
+        }
+    }
+}
